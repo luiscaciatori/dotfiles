e67ab68b2bde9058a6ce927e31cb900f28ebc52f
Luis Gustavo Caciatori <lca@starfish.team>
2024-10-19 09:54:11 -0300

    add neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+-- user settings
+--
+require("config.lazy")
+require("config.general")
+require("config.keybindings")
+require("config.telescope")
---
+++ nvim/lua/config/general.lua
@@ -0,0 +1,9 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.hlsearch = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+
+vim.cmd.colorscheme('tokyonight-storm')
+
